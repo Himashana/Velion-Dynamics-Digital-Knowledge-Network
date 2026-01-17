76833a7d402255c1be7119f3ea85753eeaaf7528
Himashana <himashanasuraweera5@gmail.com>
2026-01-17 08:16:13 +0530

    fix: updated the init database with a admin user

--- dkn-backend/src/main/resources/data.sql
+++ dkn-backend/src/main/resources/data.sql
@@ -1,3 +1,5 @@
+-- This script initializes the database with a default administrator user.
+
 INSERT INTO APP_USER (ADDRESS, CONTACT_NUMBER, DESIGNATION, EMAIL, NAME, OFFICE_LOCATION, PASSWORD, PERMISSION_LEVEL) VALUES 
 ('Gonulla, Gonawila, Sri Lanka', '0701517713', 'Administrator', 'himashanasuraweera5@gmail.com', 'Himashana Suraweera', 'Head Office, No 123, Kurunagale, Sri Lanka', '$2a$10$YAjQ3ly2GR8hoRzRDywvC.IIIwU0CC7jvXzsfZFhw8oYPg5U50Z6y', 1);
 
@@ -12,5 +14,5 @@ INSERT INTO APP_USER (ADDRESS, CONTACT_NUMBER, DESIGNATION, EMAIL, NAME, OFFICE_
 --   "performanceMetrics": {},
 --   "designation": "Administrator",
 --   "officeLocation": "Head Office, No 123, Kurunagale, Sri Lanka",
---   "responsibility": "string"
+--   "responsibility": "Manage System Users"
 -- }
